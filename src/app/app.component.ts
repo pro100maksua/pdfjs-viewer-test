1708d6e2646fcb80482ac3852dce44900c7a5616
MaksymKoloda <maksim741@ukr.net>
2021-03-21 22:31:11 +0200

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  pdfSrc = '/assets/sample.pdf';
+  pdfSrc = '../assets/sample.pdf';
 }
