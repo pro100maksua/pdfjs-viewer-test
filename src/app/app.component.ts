6f4f42fb21d70c2a1633186abd4cd9e792572db8
MaksymKoloda <maksim741@ukr.net>
2021-03-21 22:09:17 +0200

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<ng2-pdfjs-viewer pdfSrc="/assets/sampl.pdf"></ng2-pdfjs-viewer>`,
+  template: `<ng2-pdfjs-viewer pdfSrc="/assets/sample.pdf"></ng2-pdfjs-viewer>`,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
